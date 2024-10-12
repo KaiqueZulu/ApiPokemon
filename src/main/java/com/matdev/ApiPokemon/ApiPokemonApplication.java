d7f7e829e193604454e76b98aa0e05d2207d83ad
Kaique Pires <kaiqueroberto.pires@gmail.com>
2024-10-12 17:13:08 -0300

    first commit

---
+++ src/main/java/com/matdev/ApiPokemon/ApiPokemonApplication.java
@@ -0,0 +1,14 @@
+package com.matdev.ApiPokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonApplication.class, args);
+	}
+
+}
+
